9291b438d553559063f10157efd029d5b852c427
gts <gengtangsheng@outlook.com>
2022-03-27 14:39:32 +0100

    initial commit

---
+++ src/main/java/com/ibm/ixn/IxnApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.ixn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IxnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IxnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ibm/ixn/IxnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.ixn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IxnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
