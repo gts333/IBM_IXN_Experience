da413f41e2b99e8aa07871dd537cc7b88cdad7d3
gts <gengtangsheng@outlook.com>
2022-04-06 23:19:40 +0100

    fix bug on error handling

--- src/main/java/com/uclibm/ixn/controller/ErrorPageController.java
+++ src/main/java/com/uclibm/ixn/controller/ErrorPageController.java
@@ -6,12 +6,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ErrorPageController {
+
     @RequestMapping("/error/{status}")
     public String errorPage(@PathVariable Integer status){
         switch (status){
-            case 404:return "/error/404";
-            case 500:return "/error/500";
-            default:return "/error/default";
+            case 404:return "error/404";
+            case 500:return "error/500";
+            default:return "error/defaultError";
         }
     }
 }
